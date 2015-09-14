bf439831fd04ccedd3f00ba3db9726b86417a962
Cameron Basham <cbasham@gm.slc.edu>
2015-09-14 09:00:34 -0700

    mess up ability to load staticfiles in templates

--- one_shot/settings.py
+++ one_shot/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'user_extension',
     'compressor',
 )
@@ -109,10 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_URL = "/static/"
 STATIC_ROOT = "/assets/"
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    "/one_shot/assets",
 )
 
 
